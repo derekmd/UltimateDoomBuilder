643a666b67fd2015139a61582bc93ba6314b96c1
Derek <derekmd@hotmail.com>
2024-12-08 04:24:40 -0500

    TMP Texture error checker for "Door close"
    And Ceiling Lower to Floor (or 8 above?)

---
+++ Source/Plugins/BuilderModes/ErrorChecks/ActionDoorCloseTextures.cs
@@ -0,0 +1,40 @@
+﻿
+#region ================== Namespaces
+
+using CodeImp.DoomBuilder.Config;
+using CodeImp.DoomBuilder.Map;
+
+#endregion
+
+namespace CodeImp.DoomBuilder.BuilderModes
+{
+	public class ActionDoorCloseTextures : BaseActionTextures
+	{
+		#region ================== Methods
+
+		// Gather the door close and ceiling lower to floor specials from the configuration.
+		protected override bool InspectsAction(LinedefActionInfo info)
+		{
+			return info.ErrorCheckerExemptions.DoorClose;
+		}
+
+		// Determine whether an upper texture is needed after the sector ceiling lowers to the floor.
+		protected override bool HasAdjustedSector(Sidedef side, ActionTrigger actiontrigger)
+		{
+			int destinationheight = side.Sector.FloorHeight;
+
+			// Find height of the neighbouring sector with the lowest floor.
+			foreach (Sidedef s in side.Sector.Sidedefs)
+			{
+				if (s.Other != null && s.Other.Sector != side.Sector && s.Other.Sector.CeilHeight > destinationheight)
+				{
+					return true;
+				}
+			}
+
+			return false;
+		}
+
+		#endregion
+	}
+}
